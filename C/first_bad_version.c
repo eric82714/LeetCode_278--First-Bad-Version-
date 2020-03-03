122c4400bc1c31418d73130169eb6760c21a27c0
eric82714 <0958820714ab@gmail.com>
2020-03-03 20:42:06 +0800

    Create first_bad_version.c

---
+++ C/first_bad_version.c
@@ -0,0 +1,15 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    long left = 1, right = n;
+        
+    while(left < right) {
+        if(isBadVersion((left + right) / 2)) right = (left + right) / 2;
+        else {
+            if(isBadVersion((left + right) / 2+1)) return (left + right) / 2+1;
+            left = (left + right) / 2;
+        }
+    }
+    return left;
+}
